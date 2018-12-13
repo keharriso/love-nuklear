569591e6eb730f412ac7f671ebfffe0f68d5097f
Kevin Harrison <keharriso@gmail.com>
2018-12-13 16:03:52 -0500

    Add button to transform example

--- example/transform.lua
+++ example/transform.lua
@@ -6,9 +6,11 @@ return function(ui)
 	ui:rotate(t / 8)
 	ui:scale(1 + math.sin(t / 4) / 2, 1 + math.cos(t / 4) / 2)
 	ui:shear(math.cos(t / 8) / 4, math.sin(t / 8) / 4)
-	if ui:windowBegin('Transform', 0, 0, 200, 200, 'border', 'title') then
-		ui:layoutRow('dynamic', 150, 1)
+	if ui:windowBegin('Transform', 0, 0, 200, 200, 'border', 'movable', 'title') then
+		ui:layoutRow('dynamic', 100, 1)
 		ui:label('You can apply transformations to the UI using ui:rotate, ui:scale, ui:shear, and ui:translate.', 'wrap')
+		ui:layoutRow('dynamic', 30, 1)
+		ui:button('Try and catch me!')
 	end
 	ui:windowEnd()
 end
