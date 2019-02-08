0d14e1c2cfe056e00453e6207047d0d41f9aac97
Kevin Harrison <keharriso@gmail.com>
2019-02-08 12:54:19 -0500

    Update draw example: use colors in [0,1] instead of [0,255]

--- example/draw.lua
+++ example/draw.lua
@@ -5,7 +5,7 @@ local img = love.graphics.newImage 'skin/button.png'
 return function (ui)
 	if ui:windowBegin('Draw Example', 300, 300, 200, 200, 'title', 'movable', 'border') then
 		local x, y, w, h = ui:windowGetBounds()
-		love.graphics.setColor(255, 0, 0)
+		love.graphics.setColor(1, 0, 0)
 		ui:line(x + 10, y + 40, x + 50, y + 40, x + 50, y + 80)
 		ui:curve(x + 50, y + 80, x + 80, y + 40, x + 100, y + 80, x + 80, y + 80)
 		ui:polygon('line', x + 100, y + 150, x + 60, y + 140, x + 70, y + 70)
@@ -13,7 +13,7 @@ return function (ui)
 		ui:ellipse('fill', x + 30, y + 150, 20, 40)
 		ui:arc('fill', x + 150, y + 80, 40, 3 * math.pi / 2, 2 * math.pi);
 		ui:rectMultiColor(x + 95, y + 50, 50, 50, '#ff0000', '#00ff00', '#0000ff', '#000000')
-		love.graphics.setColor(255, 255, 255)
+		love.graphics.setColor(1, 1, 1)
 		ui:image(img, x + 120, y + 120, 70, 50)
 		ui:text('DRAW TEXT', x + 15, y + 75, 100, 100)
 	end
